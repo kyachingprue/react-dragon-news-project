26b0220c8ffea57baf145e15903bf14c551be632
kyachingprue <kyachingpruemarma.info@gmail.com>
2025-07-02 12:11:02 +0600

    new update commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 10000, // এখন 1000KB না হওয়া পর্যন্ত warning দেখাবে না
+  },
   plugins: [react(), tailwindcss()],
 });
